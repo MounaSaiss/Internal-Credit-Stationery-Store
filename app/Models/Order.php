e59902fbdf9c18cf0a3f13dcf8c54b6e91a34840
unknown <ismailbenmezi300@gmail.com>
2026-02-03 15:24:40 +0100

    add table (order_items)

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Order extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'status',
-        'product_id',
         'user_id',
     ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
