49c6c497e66c70135cbd4e8fec5ecc840498a3f7
unknown <ismailbenmezi300@gmail.com>
2026-02-03 10:42:12 +0100

    creat database

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->paragraphs(3, true),
+            'image'=> fake()->imageUrl(800 , 600 , 'news'),
+            'price'=> fake()->numberBetween(0 , 1000),
+            'stock'=> fake()->numberBetween(0 , 100),
+            'type'=> fake()->randomElement(['premium' , 'standard']),
+        ];
+    }
+}
