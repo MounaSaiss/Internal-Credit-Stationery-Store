6694b66715d5c85b29fca0ef434000f7b8b9a702
mourad kadiri <mouradkadi2001@gmail.com>
2026-02-04 12:05:00 +0100

    Routes changes

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<<<< Temporary merge branch 1
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-=========
+
 Route::get('/AdminStatdashboard', function () {
     return view('dashboard');
 });
 
   
->>>>>>>>> Temporary merge branch 2
